0af0afb40c96bbf49d020e3bcd3daedb6b94839d
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2025-06-27 11:57:13 -0300

    fix: deploy

--- .vercel/output/functions/__nitro.func/chunks/build/app-styles.CW6q9oL1.mjs
+++
@@ -1,6 +0,0 @@
-import { a as app_vue_vue_type_style_index_0_lang } from './entry-styles-3.mjs-hvvPXvQv.mjs';
-
-const appStyles_CW6q9oL1 = [app_vue_vue_type_style_index_0_lang, app_vue_vue_type_style_index_0_lang];
-
-export { appStyles_CW6q9oL1 as default };
-//# sourceMappingURL=app-styles.CW6q9oL1.mjs.map
